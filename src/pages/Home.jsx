82c6f676c6e0dda4a4e31d38d6d8818fbd951911
laradrb <laradarriba@hotmail.com>
2024-09-06 09:46:49 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Body } from "../styled/StyledHome";
+
+const Home = () => {
+    return (
+      <Body>
+        
+      </Body>
+    );
+  };
+  
+  export default Home;
---
+++ src/styled/StyledHome.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+  background-color: #ffffff;
+  color: white;
+  padding: 20px;
+  box-sizing: border-box;
+  margin: 0;
+
+  @media (max-width: 768px) {
+  }
+`;
+
+export const Logo = styled.img`
+  width: 80px;
+  height: 80px;
+  margin-top: 50px;
+`;
+
+
+
